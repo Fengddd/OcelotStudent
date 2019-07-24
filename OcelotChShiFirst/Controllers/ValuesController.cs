39803ae235ede95b2b4d0644a1dbb43aab0d0e0c
libenbo <1174798579@qq.com>
2019-07-24 17:06:19 +0800

    添加项目文件。

---
+++ OcelotChShiFirst/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace OcelotChShiFirst.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "OcelotChShiFirst", "OcelotChShiFirst" };
+        }
+
+        /// <summary>
+        /// 请求聚合
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<string> AggregateRootGoods(int id)
+        {
+            var item = new Goods
+            {
+                Id = id,
+                Content = $"{id}的关联的商品明细",
+            };
+            return JsonConvert.SerializeObject(item);
+        }
+        public class Goods
+        {
+            public int Id { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
---
+++ OcelotChShiTwo/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace OcelotChShiTwo.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "OcelotChShiTwo", "OcelotChShiTwo" };
+        }
+
+        /// <summary>
+        /// 请求聚合
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<string> AggregateRootOrder(int id)
+        {
+            var item = new Orders
+            {
+                Id = id,
+                Content = $"{id}的订单明细",
+            };
+            return JsonConvert.SerializeObject(item);
+        }
+
+        public class Orders
+        {
+            public int Id { get; set; }
+            public string Content { get; set; }
+        }
+
+    }
+}
